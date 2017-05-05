ce48e5a31d3090e789cb764a9aa41e53e4d52cd1
AntoineLacoste <antoine.lacoste@ynov.com>
2017-05-05 14:40:54 +0200

    commit

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,12 +2,17 @@
 cd /tmp
 
 #Clone or pull if exist (auto update on restart container)
-if cd Front_Docker_Angular; then git pull; else git clone https://github.com/EtienneTr/Front_Docker_Angular; fi
+if [ -d "docker_imgur_nodejs" ]; then 
+	cd docker_imgur_nodejs
+	git pull; 
+else 
+	git clone https://github.com/AntoineLacoste/docker_imgur_nodejs
+	cd docker_imgur_nodejs;
+fi
 
 #mongo
 exec /etc/init.d/mongod start "$@" &
 sleep 10
 
-cd Front_Docker_Angular
 npm install
 npm start
